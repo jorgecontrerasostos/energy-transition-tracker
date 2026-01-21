024e90c1353eb859793f2d7e5a6a6150bbd4a42c
Jorge Contreras <jorgecontrerasostos@gmail.com>
2026-01-20 18:26:40 -0600

    feat: add gold schema

---
+++ models/gold/gold_renewable_share_2024.sql
@@ -0,0 +1,26 @@
+with generation_data as (
+    select
+        area,
+        iso_3_code,
+        sum(case when variable = 'Renewables' then value end) as renewables_twh,
+        sum(case when variable = 'Total Generation' then value end) as total_generation_twh
+    from {{ ref("silver_global_energy_stats_clean") }}
+    where
+        year = 2024 and
+        area_type != 'Region' and
+        category = 'Electricity generation' and
+        unit = 'TWh'
+    group by
+        area,
+        iso_3_code
+)
+
+
+select
+    area,
+    iso_3_code,
+    renewables_twh,
+    total_generation_twh,
+    (renewables_twh * 100) / total_generation_twh as renewables_pct_generation
+from generation_data
+order by renewables_pct_generation desc
